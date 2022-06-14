5565e68775ed0e3714a89209ba31a45794796dad
MarinaB83 <93393428+MarinaB83@users.noreply.github.com>
2022-06-14 17:40:46 +0400

    new

---
+++ os_homework.py
@@ -0,0 +1,21 @@
+import os
+
+print(os.getcwd())
+print(os.listdir())
+
+
+# fp = open("file.txt", "w")
+# fp.write("hello python")
+# fp.close()
+
+
+# name = input("tell the name of the folder: ")
+# os.mkdir(f"{os.getcwd()}/{name}")
+# os.mkdir(f"{os.getcwd()}/{name}/Dir2")
+# os.mkdir(f"{os.getcwd()}/{name}/Dir3")
+# os.makedirs("/Users/PC/Desktop/git_files/python_classes/dir1/Dir3/Dir4")
+
+
+
+
+
